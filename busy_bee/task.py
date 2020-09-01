bb1eb662de4e16290eb1fc9721b460dc4c429068
Lars Hillebrand <Lars.Patrick.Hillebrand@iais.fraunhofer.de>
2020-09-01 11:35:25 +0200

    Some bug fixes to busybee.work().

--- busy_bee/task.py
+++ busy_bee/task.py
@@ -1,12 +1,24 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
 
 
 class Task(ABC):
     """ Abstract class for tasks
     """
-    def __init__(self, id_: int, pre_task_ids: List[int] = [],
-                 post_task_ids: List[int] = []):
+    def __init__(self,
+                 id_: int,
+                 # name: Optional[str] = None,
+                 pre_task_ids: Optional[List[int]] = None,
+                 post_task_ids: Optional[List[int]] = None):
+        if pre_task_ids is None:
+            pre_task_ids = []
+        if post_task_ids is None:
+            post_task_ids = []
+
+        # if name is None:
+        #     name = self.__name__
+        #
+        # self.name = name
         self.id_ = id_
         self.pre_task_ids = pre_task_ids
         self.post_task_ids = post_task_ids
