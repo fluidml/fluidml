09f40ebac2ded12204b56c62653969ee715231dd
Rajkumar R <raj1514@gmail.com>
2020-09-02 18:58:12 +0200

    Some cleanup

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import setup, find_packages
 setup(name="busy_bee",
       version="0.0.1",
       description="A minimal framework for parallelizing tasks in python",
-      author="Rajkumar Ramamurthy",
+      author="Rajkumar Ramamurthy, Lars Hillebrand",
       author_email="raj1514@gmail.com",
       packages=find_packages(),
-      install_requires=["networkx",
-                        "pyyaml",
-                        "rich",
-                        "torch"])
+      install_requires=["rich"],
+      extras_require={'demo': ["pyyaml", "torch", "flair", "pytorch-nlp"]})
