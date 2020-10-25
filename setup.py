5c40aa885f1fcd28e16db610c45560a9088f0327
Lars Hillebrand <larshillebrand@icloud.com>
2020-10-25 13:58:12 +0100

    Updated meta files.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name="fluidml",
       version="0.0.1",
-      description="A minimal framework for parallelizing tasks in python",
+      description="A minimal framework for parallelizing machine learning tasks in python.",
       author="Rajkumar Ramamurthy, Lars Hillebrand",
       author_email="raj1514@gmail.com",
       packages=find_packages(),
