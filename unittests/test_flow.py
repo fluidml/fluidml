8ab6c5deff62914631fc2b5728166534c053da8c
Rajkumar Ramamurthy <raj1514@gmail.com>
2020-10-26 18:34:59 +0100

    Integrated circleCI support and test coverage
    Added simple unit tests
    
    Added missing init file
    
    For saving test reports
    
    To store code coverage
    
    Added pytest
    
    Fixed config to write test reports
    
    FIxed artifacts path
    
    Yet another fix
    
    Fix paths

---
+++ unittests/test_flow.py
@@ -0,0 +1,16 @@
+import pytest
+from fluidml.flow import Flow
+from fluidml.swarm import Swarm
+
+
+@pytest.fixture
+def swarm() -> Swarm:
+    swarm = Swarm(n_dolphins=4)
+    yield swarm
+    swarm.close()
+
+
+def test_flow_instantiation(swarm):
+    flow = Flow(swarm=swarm)
+    flow.run([])
+    assert True
