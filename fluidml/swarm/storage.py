181c89741d1a6424266cccd03e95e54e5a2e0b79
Lars Hillebrand <larshillebrand@icloud.com>
2020-10-31 22:04:03 +0100

    Saved results as pickle to avoid json serialization limitations.

--- fluidml/swarm/storage.py
+++ fluidml/swarm/storage.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import json
 import os
+import pickle
 from shutil import rmtree
 from typing import List, Dict, Optional, Tuple
 
@@ -40,7 +41,7 @@ class LocalFileStorage(ResultsStorage):
         exist_run_dirs = LocalFileStorage._scan_task_dir(task_dir=task_dir)
         run_dir = LocalFileStorage._get_run_dir(task_config=unique_config, exist_run_dirs=exist_run_dirs)
         if run_dir:
-            result = json.load(open(os.path.join(run_dir, 'result.json'), 'r'))
+            result = pickle.load(open(os.path.join(run_dir, 'result.p'), 'rb'))
             return result, run_dir
         return None
 
@@ -50,7 +51,7 @@ class LocalFileStorage(ResultsStorage):
 
         task_history = {name: sorted(path) for name, path in history.items()}
         json.dump(task_history, open(os.path.join(run_dir, 'info.json'), 'w'))
-        json.dump(results, open(os.path.join(run_dir, 'result.json'), 'w'))
+        pickle.dump(results, open(os.path.join(run_dir, 'result.p'), 'wb'))
         json.dump(unique_config, open(os.path.join(run_dir, 'config.json'), 'w'))
         return run_dir
 
@@ -67,7 +68,7 @@ class LocalFileStorage(ResultsStorage):
         # save new task results
         task_history = {name: sorted(path) for name, path in history.items()}
         json.dump(task_history, open(os.path.join(run_dir, 'info.json'), 'w'))
-        json.dump(results, open(os.path.join(run_dir, 'result.json'), 'w'))
+        pickle.dump(results, open(os.path.join(run_dir, 'result.p'), 'wb'))
         json.dump(unique_config, open(os.path.join(run_dir, 'config.json'), 'w'))
         return run_dir
 
@@ -88,7 +89,7 @@ class LocalFileStorage(ResultsStorage):
                 elif element.is_dir():
                     rmtree(element.path)
             except OSError as e:
-                print(f'Failed to delete {element.path}. Reason: {e}.')
+                print(f'Failed to delete content in {element.path}. Reason: {e}.')
 
     @staticmethod
     def _get_run_dir(task_config: Dict, exist_run_dirs: List[str]) -> Optional[str]:
