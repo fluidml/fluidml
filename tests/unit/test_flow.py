a82ac18e2ee8d7dc38f658e5e313c94337a84983
Rajkumar R <raj1514@gmail.com>
2020-11-02 18:51:53 +0100

    Added simple test for TDD of lambda support

--- tests/unit/test_flow.py
+++ tests/unit/test_flow.py
@@ -3,6 +3,7 @@ import pytest
 from fluidml.flow import Flow
 from fluidml.swarm import Swarm
 from fluidml.common.exception import NoTasksError
+from fluidml.flow.task_spec import TaskSpec
 
 
 @pytest.fixture
@@ -16,3 +17,12 @@ def test_flow_with_no_tasks(swarm):
     with pytest.raises(NoTasksError):
         flow = Flow(swarm=swarm)
         flow.run([])
+
+
+def test_flow_with_lambda(swarm):
+    task_spec_a = TaskSpec(task=lambda results, resource, x: {}, task_kwargs={"x": 1})
+    task_spec_b = TaskSpec(task=lambda results, resource, x: {}, task_kwargs={"x": 1})
+    task_spec_b.requires([task_spec_a])
+    flow = Flow(swarm)
+    results = flow.run([task_spec_a, task_spec_b])
+    assert len(results) == 2
