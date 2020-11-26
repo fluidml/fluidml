1029baeb2154d11c238a8f568529ab0acbc10fb5
Rajkumar R <raj1514@gmail.com>
2020-11-26 21:52:49 +0100

    Refactored worker code

---
+++ fluidml/swarm/orca.py
@@ -0,0 +1,31 @@
+import time
+from typing import Dict, List
+
+from rich.progress import Progress, BarColumn
+
+from fluidml.common.task import Task
+from fluidml.swarm.whale import Whale
+
+
+class Orca(Whale):
+    def __init__(self,
+                 done_queue: List[int],
+                 exception: Dict[str, Exception],
+                 exit_on_error: bool,
+                 tasks: Dict[int, Task],
+                 refresh_every: int):
+        super().__init__(exception=exception, exit_on_error=exit_on_error)
+        self.refresh_every = refresh_every
+        self.done_queue = done_queue
+        self.tasks = tasks
+
+    def _work(self):
+        while len(self.done_queue) < len(self.tasks) and not self.exception:
+            # sleep for a while
+            time.sleep(self.refresh_every)
+
+            with Progress('[progress.description]{task.description}', BarColumn(),
+                          '[progress.percentage]{task.percentage:>3.0f}%',) as progress:
+
+                task = progress.add_task('[red]Task Progress...', total=len(self.tasks))
+                progress.update(task, advance=len(self.done_queue))
