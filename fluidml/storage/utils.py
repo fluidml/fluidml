da7ede340aa623fe6bb64024aba748d27b15ab59
Lars Hillebrand <larshillebrand@icloud.com>
2020-12-06 15:42:44 +0100

    Added unique task configs to results. Returns results based on boolean.

--- fluidml/storage/utils.py
+++ fluidml/storage/utils.py
@@ -1,21 +1,31 @@
-from typing import Dict, Any, List, Tuple
-from fluidml.storage.base import ResultsStore
 from collections import defaultdict
+from typing import Dict, Any, List, Tuple
+
 from fluidml.common.exception import TaskResultTypeError
+from fluidml.storage import ResultsStore
 
 
-def pack_results(results_store: ResultsStore, task_configs: List[Tuple[str, Dict]]) -> Dict[str, Any]:
+def pack_results(results_store: ResultsStore,
+                 task_configs: List[Tuple[str, Dict]],
+                 return_results: bool = True) -> Dict[str, Any]:
     results = defaultdict(list)
-    for task_name, task_config in task_configs:
-        result = results_store.get_results(task_name, task_config)
-        if isinstance(result, dict):
-            results[task_name].append(result)
-        else:
-            raise TaskResultTypeError("Each task has to return a dict")
+    if return_results:
+        for task_name, task_config in task_configs:
+            result = results_store.get_results(task_name, task_config)
+            if isinstance(result, dict):
+                results[task_name].append({'result': result,
+                                           'config': task_config})
+            else:
+                raise TaskResultTypeError("Each task has to return a dict")
+    else:
+        for task_name, task_config in task_configs:
+            results[task_name].append(task_config)
 
-    # simplify results
+    return simplify_results(results=results)
+
+
+def simplify_results(results: Dict[str, Any]) -> Dict[str, Any]:
     for task_name, task_results in results.items():
         if len(task_results) == 1:
             results[task_name] = task_results[0]
-
     return results
