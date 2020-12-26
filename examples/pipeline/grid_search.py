ae1c19bc438d3e47a44ae250a8dc871b8549dddb
Lars Hillebrand <larshillebrand@icloud.com>
2020-12-26 21:18:36 +0100

    Merged both notebook branches.

--- examples/pipeline/grid_search.py
+++ examples/pipeline/grid_search.py
@@ -10,7 +10,7 @@ from fluidml.common import Resource
 from fluidml.flow import Flow
 from fluidml.flow import GridTaskSpec, TaskSpec
 from fluidml.swarm import Swarm
-from fluidml.storage import LocalFileStore, MongoDBStore
+from fluidml.storage import LocalFileStore, MongoDBStore, InMemoryStore
 
 
 def parse(results: Dict, resource: Resource, in_dir: str):
@@ -58,7 +58,7 @@ parse -> preprocess -> featurize_tokens -> train -> evaluate
 parse --> preprocess_1 -> featurize_tokens_1 ----> train -> evaluate (reduce grid search)
                        \- featurize_tokens_2 --\/
                        \- featurize_cells    --/\> train -/
-                      
+
       \-> preprocess_2 -> featurize_tokens_1 ----> train -/
                        \- featurize_tokens_2 --\/
                        \- featurize_cells    --/\> train -/
@@ -133,8 +133,8 @@ def main():
                               seed=args.seed) for i in range(args.num_dolphins)]
 
     # create local file storage used for versioning
-    # results_store = LocalFileStore(base_dir=args.base_dir)
-    results_store = MongoDBStore("test3")
+    results_store = LocalFileStore(base_dir=args.base_dir)
+    # results_store = MongoDBStore("test3")
 
     # run tasks in parallel (GridTaskSpecs are expanded based on grid search arguments)
     with Swarm(n_dolphins=args.num_dolphins,
@@ -147,3 +147,63 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# def parse(results: Dict, in_dir: str):
+#     return {}
+#
+#
+# def preprocess(results: Dict, pipeline: List[str], abc: List[int]):
+#     return {}
+#
+#
+# def featurize_tokens(results: Dict, type_: str, batch_size: int):
+#     return {}
+#
+#
+# def featurize_cells(results: Dict, type_: str, batch_size: int):
+#     return {}
+#
+#
+# def train(results: Dict, model, dataloader, evaluator, optimizer, num_epochs):
+#     return {'score': 2.}  # 'score': 2.
+#
+#
+# def evaluate(results: Dict, metric: str):
+#     print(results)
+#     return {}
+#
+#
+# CURRENT_DIR = os.getcwd()
+# base_dir = os.path.join(CURRENT_DIR, 'experiments')
+#
+# parse_task = GridTaskSpec(task=parse, gs_config={"in_dir": "/some/dir"})
+# preprocess_task = GridTaskSpec(task=preprocess, gs_config={"pipeline": ['a', 'b'], "abc": [1, 2]})
+# featurize_tokens_task = GridTaskSpec(task=featurize_tokens, gs_config={"type_": "flair", 'batch_size': 2})
+# featurize_cells_task = GridTaskSpec(task=featurize_cells, gs_config={"type_": "glove", "batch_size": 4})
+# train_task = GridTaskSpec(task=train, gs_config={"model": "mlp", "dataloader": "x", "evaluator": "y", "optimizer": "adam", "num_epochs": 10})
+# evaluate_task = TaskSpec(task=evaluate, reduce=True, task_kwargs={"metric": "accuracy"})
+#
+# # dependencies between tasks
+# preprocess_task.requires([parse_task])
+# featurize_tokens_task.requires([preprocess_task])
+# featurize_cells_task.requires([preprocess_task])
+# train_task.requires([featurize_tokens_task, featurize_cells_task])
+# evaluate_task.requires([train_task])
+#
+# # all tasks
+# tasks = [parse_task,
+#          preprocess_task,
+#          featurize_tokens_task,
+#          featurize_cells_task,
+#          train_task,
+#          evaluate_task]
+#
+# results_store = LocalFileStore(base_dir=CURRENT_DIR)
+#
+# # run tasks in parallel (GridTaskSpecs are expanded based on grid search arguments)
+# if __name__ == '__main__':
+#     with Swarm(n_dolphins=4, results_store=results_store) as swarm:
+#         flow = Flow(swarm=swarm)
+#         results = flow.run(tasks)
+#         print(results)
