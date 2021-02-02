b74eecda0c855ce933a98dd793944266c8431c93
Rajkumar Ramamurthy <raj1514@gmail.com>
2021-02-02 22:53:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(name='fluidml',
       version='0.1.0',
       description='A minimal framework for parallelizing machine learning tasks in python.',
-      long_description=open('README.md').read(),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Rajkumar Ramamurthy, Lars Hillebrand',
       author_email='raj1514@gmail.com',
       packages=find_packages(),
