1f9905b4b7cb7c7b78a7c2f2d36abd32e8e70bef
Rajkumar Ramamurthy <raj1514@gmail.com>
2021-02-02 22:40:52 +0100

    Pip workflow (#34)
    * Create main.yml
    * Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(name='fluidml',
       version='0.1.0',
       description='A minimal framework for parallelizing machine learning tasks in python.',
+      long_description=open('README.md').read(),
       author='Rajkumar Ramamurthy, Lars Hillebrand',
       author_email='raj1514@gmail.com',
       packages=find_packages(),
