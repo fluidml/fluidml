d117df27d15ea0262761d6d68099e104d728e24c
Lars Hillebrand <larshillebrand@icloud.com>
2021-03-04 18:17:44 +0100

    Implemented console task graph visualization. Decoupled flow.create() and flow.run(). Improved force arg usage.

--- fluidml/flow/flow.py
+++ fluidml/flow/flow.py
@@ -1,16 +1,26 @@
 from collections import defaultdict
 import logging
 from itertools import product
-from typing import List, Any, Dict, Optional
+import pydoc
+from typing import List, Any, Dict, Optional, Set, Union
 
+import networkx as nx
 from networkx import DiGraph
 from networkx.algorithms.dag import topological_sort
 
 from fluidml.common import Task
 from fluidml.common.utils import update_merge, reformat_config
-from fluidml.common.exception import NoTasksError
+from fluidml.common.exception import NoTasksError, CyclicGraphError
 from fluidml.flow import BaseTaskSpec, GridTaskSpec
+from fluidml.flow.ascii_graph_visualization import draw
 from fluidml.swarm import Swarm
+
+
+try:
+    from rich.traceback import install
+    install(extra_lines=2)
+except ImportError:
+    pass
 
 
 logger = logging.getLogger(__name__)
@@ -21,68 +31,117 @@ class Flow:
 
     - It automatically expands the tasks based on task spec and task config
     - It extends the dependencies to the expanded tasks
+    - It provides the task graph objects and simple console graph visualization
     - Finally, it composes a list of tasks which are then run through the provided swarm
     """
 
-    def __init__(self,
-                 swarm: Swarm,
-                 task_to_execute: Optional[str] = None,
-                 force: Optional[str] = None):
+    def __init__(self, swarm: Swarm):
         """
         Args:
             swarm (Swarm): an instance of the swarm
-            task_to_execute (Optional[str], optional): a list of task names only those tasks which needs to be run 
-            force (Optional[str], optional): forcefully re-run tasks
-                Possible options are:
-                    "selected" - Only specified tasks in task_to_execture are re-run
-                    "all" - All the tasks are re-run
         """
         self._swarm = swarm
-        self._task_to_execute = task_to_execute
-        self._force = force
+
+        # self._task_to_execute: Optional[str] = None
+
+        # contains the expanded graph as list of Task objects -> used internally in swarm
+        self._expanded_tasks: Optional[List[Task]] = None
+        # contains the expanded graph as networkx DiGraph -> accessible to the user for introspection and visualization
+        self.task_graph: Optional[DiGraph] = None
+        # contains the original, user defined task spec graph as networkx DiGraph
+        # -> accessible to the user for introspection and visualization
+        self.task_spec_graph: Optional[DiGraph] = None
 
     @staticmethod
-    def _create_task_spec_graph(task_specs: List[BaseTaskSpec]) -> DiGraph:
-        task_spec_graph = DiGraph()
-        for spec in task_specs:
-            task_spec_graph.add_node(spec.name)
-            for predecessor in spec.predecessors:
-                task_spec_graph.add_edge(predecessor.name, spec.name)
+    def _check_acyclic(graph: DiGraph) -> None:
+
+        # try to find cycles in graph -> return None if None are found
+        try:
+            edges = nx.find_cycle(graph, orientation='original')
+        except nx.NetworkXNoCycle:
+            return
+
+        # gather nodes involved in cycle and raise an error
+        nodes_containing_cycle: Set[str] = set()
+        for from_node, to_node, _ in edges:
+            nodes_containing_cycle.add(from_node)
+            nodes_containing_cycle.add(to_node)
+
+        msg = f'Pipeline has a cycle involving: {", ".join(list(nodes_containing_cycle))}.'
+        raise CyclicGraphError(msg)
+
+    @staticmethod
+    def _create_graph_from_task_list(tasks: List[Union[BaseTaskSpec, Task]], name: Optional[str] = None) -> DiGraph:
+        """ Creates nx.DiGraph object of the list of defined tasks with registered dependencies."""
+        graph = DiGraph()
+        for task in tasks:
+            graph.add_node(task.unique_name, task=task)
+            for predecessor in task.predecessors:
+                graph.add_node(predecessor.unique_name, task=predecessor)
+                graph.add_edge(predecessor.unique_name, task.unique_name)
+        if name is not None:
+            graph.name = name
+        return graph
+
+    def _create_task_spec_graph(self, task_specs: List[BaseTaskSpec]) -> DiGraph:
+        task_spec_graph = Flow._create_graph_from_task_list(tasks=task_specs, name='task spec graph')
+
+        # assure that task spec graph contains no cyclic dependencies
+        Flow._check_acyclic(graph=task_spec_graph)
+        self.task_spec_graph = task_spec_graph
         return task_spec_graph
 
-    def _register_tasks_to_force_execute(self, tasks: List[Task]):
-        if self._force == 'selected':
-            assert self._task_to_execute is not None, '"task_to_execute" is required for force = "selected".'
-            for task in tasks:
-                if task.name == self._task_to_execute:
-                    task.force = True
-        elif self._force == 'all':
-            for task in tasks:
+    def _register_tasks_to_force_execute(self, force: Union[str, List[str]]) -> None:
+        # if force == 'all' set force to True for all tasks
+        if force == 'all':
+            for task in self._expanded_tasks:
                 task.force = True
-        elif self._force is not None:
-            raise ValueError(
-                f'force = {self._force} is not supported. Choose "all" or "selected"')
+            return
+        # convert to list if force is of type str
+        if isinstance(force, str):
+            force = [force]
+
+        if not isinstance(force, List):
+            raise TypeError(f'"force" argument has to be of type str or list of str.')
+
+        # get all user provided task names to force execute
+        force_task_names = [task_name[:-1] if task_name[-1] == '+' else task_name for task_name in force]
+        # get all task names defined in the task spec graph
+        task_names = list(self.task_spec_graph.nodes)
+        # find all user provided task names to force execute that don't exist in the task spec graph
+        unknown_task_names = list(set(force_task_names).difference(task_names))
+        # if any unknown names are found, raise a ValueError
+        if unknown_task_names:
+            raise ValueError(f'The following task names provided to "force" are unknown: '
+                             f'{", ".join(unknown_task_names)}')
+
+        # create a list of unique task names to force execute
+        # if the user adds "+" to a task names we find all successor tasks in the graph and add them to the
+        #  force execute list.
+        tasks_to_force_execute = []
+        for task_name in force:
+            if task_name[-1] == '+':
+                task_name = task_name[:-1]
+                successor_tasks = [successor
+                                   for successors in nx.dfs_successors(self.task_spec_graph, task_name).values()
+                                   for successor in successors]
+                tasks_to_force_execute.extend(successor_tasks)
+            tasks_to_force_execute.append(task_name)
+        tasks_to_force_execute = list(set(tasks_to_force_execute))
+
+        # set force == True for all tasks in the created tasks_to_force_execute list
+        for task in self._expanded_tasks:
+            if task.name in tasks_to_force_execute:
+                task.force = True
 
     def _order_task_specs(self,
-                          task_specs: List[BaseTaskSpec]) -> List[BaseTaskSpec]:
+                          task_specs: List[BaseTaskSpec],
+                          ) -> List[BaseTaskSpec]:
         # task spec graph holding the user defined dependency structure
-        task_spec_graph: DiGraph = Flow._create_task_spec_graph(
-            task_specs=task_specs)
+        task_spec_graph: DiGraph = self._create_task_spec_graph(task_specs=task_specs)
 
         # topological ordering of tasks in graph
-        # if a specific task to execute is provided, remove non dependent tasks from graph
-        if self._task_to_execute:
-            sorted_names = list(topological_sort(task_spec_graph))
-            target_idx = sorted_names.index(self._task_to_execute)
-            sorted_names = sorted_names[:target_idx + 1]
-        else:
-            sorted_names = list(topological_sort(task_spec_graph))
-
-        # convert task_specs to dict, so it can be queried by name
-        name_to_task_spec = {spec.name: spec for spec in task_specs}
-
-        # get sorted list of task specs
-        sorted_specs = [name_to_task_spec[name] for name in sorted_names]
+        sorted_specs = [task_spec_graph.nodes[task_name]['task'] for task_name in topological_sort(task_spec_graph)]
         return sorted_specs
 
     @staticmethod
@@ -134,8 +193,7 @@ class Flow:
 
     @staticmethod
     def _merge_task_combination_configs(task_combinations: List[List[Task]], task_specs: List[BaseTaskSpec]) -> Dict:
-        task_configs = [
-            task.unique_config for combination in task_combinations for task in combination]
+        task_configs = [task.unique_config for combination in task_combinations for task in combination]
         merged_config = task_configs.pop(0)
         for config in task_configs:
             merged_config: Dict = update_merge(merged_config, config)
@@ -159,7 +217,7 @@ class Flow:
         return merged_config
 
     @staticmethod
-    def _generate_tasks(task_specs: List[BaseTaskSpec]) -> List[Task]:
+    def _generate_task_graph(task_specs: List[BaseTaskSpec]) -> List[Task]:
         # keep track of expanded tasks by their names
         expanded_tasks_by_name = defaultdict(list)
         task_id = 0
@@ -167,8 +225,7 @@ class Flow:
         # for each task to expand
         for exp_task in task_specs:
             # get predecessor task combinations
-            task_combinations = Flow._get_predecessor_product(
-                expanded_tasks_by_name, exp_task)
+            task_combinations = Flow._get_predecessor_product(expanded_tasks_by_name, exp_task)
 
             if exp_task.reduce:
                 # if it is a reduce task, just add the predecessor task
@@ -177,8 +234,7 @@ class Flow:
 
                 for task in tasks:
                     # predecessor config
-                    predecessor_config = Flow._merge_task_combination_configs(
-                        task_combinations, task_specs)
+                    predecessor_config = Flow._merge_task_combination_configs(task_combinations, task_specs)
 
                     # add dependencies
                     for task_combination in task_combinations:
@@ -187,8 +243,7 @@ class Flow:
                     task.id_ = task_id
                     task.reduce = True
                     expanded_tasks_by_name[task.name].append(task)
-                    task.unique_config = {
-                        **predecessor_config, **{task.name: task.kwargs}}
+                    task.unique_config = {**predecessor_config, **{task.name: task.kwargs}}
                     task_id += 1
             else:
                 # for each combination, create a new task
@@ -196,39 +251,78 @@ class Flow:
                     tasks = exp_task.build()
 
                     # shared predecessor config
-                    predecessor_config = Flow._combine_task_config(
-                        task_combination)
+                    predecessor_config = Flow._combine_task_config(task_combination)
 
                     # for each task that is created, add ids and dependencies
                     for task in tasks:
                         task.id_ = task_id
                         task.requires(task_combination)
-                        task.unique_config = {
-                            **predecessor_config, **{task.name: task.kwargs}}
+                        task.unique_config = {**predecessor_config, **{task.name: task.kwargs}}
                         expanded_tasks_by_name[task.name].append(task)
                         task_id += 1
 
-        # create final list of tasks
-        tasks = [task for expanded_tasks in expanded_tasks_by_name.values()
-                 for task in expanded_tasks]
+        # create final list of linked tasks and set expansion id for expanded tasks
+        tasks = []
+        for expanded_tasks in expanded_tasks_by_name.values():
+            if len(expanded_tasks) == 1:
+                for task in expanded_tasks:
+                    tasks.append(task)
+            else:
+                for expansion_id, task in enumerate(expanded_tasks, 1):
+                    task.unique_name = f'{task.name}-{expansion_id}'
+                    tasks.append(task)
+
         return tasks
 
-    def run(self,
-            task_specs: List[BaseTaskSpec]) -> Dict[str, Dict[str, Any]]:
-        """Runs the specified tasks and returns the results
+    @staticmethod
+    def visualize(graph: DiGraph):
+        """Creates the task graph by expanding all GridTaskSpecs and taking reduce=True tasks into account.
+
+        Args:
+            graph (DiGraph): a networkx directed graph object
+        """
+
+        vertexes = list(graph.nodes())
+        edges = list(graph.edges())
+
+        paged_text = f'{graph.name}\n\n' if graph.name else ''
+        paged_text += f'{draw(vertexes=vertexes, edges=edges)}\n\n'
+
+        pydoc.pager(text=paged_text)
+
+    def create(self,
+               task_specs: List[BaseTaskSpec]):
+        """Creates the task graph by expanding all GridTaskSpecs and taking reduce=True tasks into account.
 
         Args:
             task_specs (List[Union[TaskSpec, GridTaskSpec]]): list of task specifications
-
-        Returns:
-            Dict[str, Dict[str, Any]]: a nested dict of results
-
         """
+
         if not task_specs:
             raise NoTasksError("There are no tasks to run")
 
-        ordered_task_specs = self._order_task_specs(task_specs)
-        tasks = Flow._generate_tasks(ordered_task_specs)
-        self._register_tasks_to_force_execute(tasks)
-        results = self._swarm.work(tasks)
+        ordered_task_specs = self._order_task_specs(task_specs=task_specs)
+        self._expanded_tasks: List[Task] = Flow._generate_task_graph(ordered_task_specs)
+        self.task_graph: DiGraph = Flow._create_graph_from_task_list(tasks=self._expanded_tasks, name='task graph')
+
+    def run(self,
+            force: Optional[Union[str, List[str]]] = None):
+        """Runs the specified tasks and returns the results
+
+        Args:
+            force (Optional[str], optional): forcefully re-run tasks
+                Possible options are:
+                    "selected" - Only specified tasks in task_to_execute are re-run
+                    "all" - All the tasks are re-run
+
+        Returns:
+            Dict[str, Dict[str, Any]]: a nested dict of results
+        """
+        if self._expanded_tasks is None:
+            raise NoTasksError('Execute "flow.create(tasks)" to build the task graph before calling "flow.run()".')
+
+        if force is not None:
+            self._register_tasks_to_force_execute(force=force)
+
+        results = self._swarm.work(tasks=self._expanded_tasks)
         return results
